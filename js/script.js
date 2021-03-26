4f3005fd2d7966a1c06e33c47f55ad45c656716e
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-03-26 11:05:41 -0600

    add script id's

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+// Business Logic
+
+
 $(document).ready(function () {
+  alert(the doc is ready")
+    
+    
   
   $("form").submit(function (event) {
     event.preventDefault();
-    
-
-
+    const bike = document.getElementById("bike").checked;
+    const water = document.getElementById("water").checked;
+    const sky = document.getElementById("sky").checked;
+    const mash = document.getElementById("mash").checked;
+    const three = document.getElementById("three").checked;
+    const taxi = document.getElementById("taxi").checked
+    const 
   });
 });
