87481b0b6b157a1c97d08db5c88e4b2608f71da4
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-03-26 15:12:58 -0600

    add divs and style to jumbotron

--- js/script.js
+++ js/script.js
@@ -2,18 +2,66 @@
 
 
 $(document).ready(function () {
-  alert(the doc is ready")
-    
-    
-  
+
+
   $("form").submit(function (event) {
+
+    alert("func running")
+
+    // $("input[name$='tv']").click(function () {
+    //   const test = $(this).val();
+    //   $("#" + test).show();
+    // });
+    // alert(click ran)
+    
+
+    const mash = $("input:radio[name=tv]:checked").val();
+    const three = $("input:radio[name=tv]:checked").val();
+    const taxi = $("input:radio[name=tv]:checked").val();
+
+    const bike = $("input:radio[name=hobby]:checked").val();
+    const water = $("input:radio[name=hobby]:checked").val();
+    const sky = $("input:radio[name=hobby]:checked").val();
+
+    const symphony = $("input:radio[name=music]:checked").val();
+    const rock = $("input:radio[name=music]:checked").val();
+    const country = $("input:radio[name=music]:checked").val();
+
+    const sushi = $("input:radio[name=food]:checked").val();
+    const chicken = $("input:radio[name=food]:checked").val();
+    const salad = $("input:radio[name=food]:checked").val();
+
+    const morning = $("input:radio[name=day]:checked").val();
+    const evening = $("input:radio[name=day]:checked").val();
+    const night = $("input:radio[name=day]:checked").val();
+
+
+  
+  
+
+
+
+    alert("const is executed")
+
+    if (bike && mash && classic && sushi && morning) {
+      $("#javaScript").show();
+      $(".answer").hide();
+    } else if (water && three && rock && chicken && evening) {
+      $("#cSharp").show();
+      // $(".answer").hide();
+    } else if (sky && taxi && country && sushi && morning) {
+      $("#javaScript").show();
+      // $(".answer").hide();
+    } else if (water && three && rock && chicken && evening) {
+      $("#cSharp").show();
+      // $(".answer").hide();
+    } else {
+      $("#cSharp").show();
+      // $(".answer").hide();
+    }
+    alert("else if have run")
+
     event.preventDefault();
-    const bike = document.getElementById("bike").checked;
-    const water = document.getElementById("water").checked;
-    const sky = document.getElementById("sky").checked;
-    const mash = document.getElementById("mash").checked;
-    const three = document.getElementById("three").checked;
-    const taxi = document.getElementById("taxi").checked
-    const 
+
   });
 });
