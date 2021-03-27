f5d9358f904b85a90a689489dd5f470aef93af64
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-03-26 18:03:27 -0600

    finish else if and redo value

--- js/script.js
+++ js/script.js
@@ -1,76 +1,23 @@
-// Business Logic
-
-
 $(document).ready(function () {
-
-
-  $("form").submit(function (event) {
-
-    alert("func running")
-
-    $("radio").click(function () {
-      $("#cSharp").show
-    });
-
-    // $("input[name$='tv']").click(function () {
-    //   const test = $(this).val();
-    //   $("#" + test).show();
-    // });
-    // alert(click ran)
-    
-
-    const mash = $("input:radio[name=tv]:checked").val();
-    const three = $("input:radio[name=tv]:checked").val();
-    const taxi = $("input:radio[name=tv]:checked").val();
-
-    const bike = $("input:radio[name=hobby]:checked").val();
-    const water = $("input:radio[name=hobby]:checked").val();
-    const sky = $("input:radio[name=hobby]:checked").val();
-
-    const symphony = $("input:radio[name=music]:checked").val();
-    const rock = $("input:radio[name=music]:checked").val();
-    const country = $("input:radio[name=music]:checked").val();
-
-    const sushi = $("input:radio[name=food]:checked").val();
-    const chicken = $("input:radio[name=food]:checked").val();
-    const salad = $("input:radio[name=food]:checked").val();
-
-    const morning = $("input:radio[name=day]:checked").val();
-    const evening = $("input:radio[name=day]:checked").val();
-    const night = $("input:radio[name=day]:checked").val();
-
-
-  
-  
-
-
-
-    alert("const is executed")
-
-    // if (bike && classic && sushi) {
-    //   $("#javaScript").show();
-    // }
-
-    if (bike && mash && classic && sushi && morning) {
-      $(".h2/#javaScript").show();
-      $(".answer").hide();
-    } else { (water && three && rock && chicken && evening) 
-      $("#cSharp").show();
-      $(".answer").hide();
-    }
-    //  else if (sky && taxi && country && sushi && morning) {
-    //   $("#javaScript").show();
-    //   // $(".answer").hide();
-    // } else if (water && three && rock && chicken && evening) {
-    //   $("#cSharp").show();
-    //   // $(".answer").hide();
-    // } else {
-    //   $("#cSharp").show();
-    //   // $(".answer").hide();
-    // }
-    // alert("else if have run")
-
+  $("form#form-one").submit(function (event) {
     event.preventDefault();
 
+    $(".answer").hide();
+
+    const tv = $("input:radio[name=tv]:checked").val();
+    const hobby = $("input:radio[name=hobby]:checked").val();
+    const music = $("input:radio[name=music]:checked").val();
+    const food = $("input:radio[name=food]:checked").val();
+    const day = $("input:radio[name=day]:checked").val();
+
+    if (hobby === "bike" && tv === "mash" && music === "rock" && food === "chicken" && day === "morning") {
+      $("#python").show();
+    } else if (tv === "three" && hobby === "water" && music === "country" && food === "salad" && day === "evening") {
+      $("#cSharp").show();
+    } else if (tv === "taxi" && hobby === "sky" && music === "symphony" && food === "sushi" && day === "night") {
+      $("#javaScript").show();
+    } else {
+      $("#cSharp").show();
+    }
   });
 });
